828b30744d317fc5a8c8e49906da66dd03beaf0e
denis.yudin <rng48322@gmail.com>
2022-09-26 19:00:00 +0300

    homework

--- src/main/java/com/edu/ulab/app/facade/UserDataFacade.java
+++ src/main/java/com/edu/ulab/app/facade/UserDataFacade.java
@@ -2,10 +2,13 @@ package com.edu.ulab.app.facade;
 
 import com.edu.ulab.app.dto.BookDto;
 import com.edu.ulab.app.dto.UserDto;
+import com.edu.ulab.app.entity.Book;
 import com.edu.ulab.app.mapper.BookMapper;
 import com.edu.ulab.app.mapper.UserMapper;
 
+import com.edu.ulab.app.service.impl.BookServiceImpl;
 import com.edu.ulab.app.service.impl.BookServiceImplTemplate;
+import com.edu.ulab.app.service.impl.UserServiceImpl;
 import com.edu.ulab.app.service.impl.UserServiceImplTemplate;
 import com.edu.ulab.app.web.request.UserBookRequest;
 import com.edu.ulab.app.web.response.UserBookResponse;
@@ -18,13 +21,13 @@ import java.util.Objects;
 @Slf4j
 @Component
 public class UserDataFacade {
-    private final UserServiceImplTemplate userService;
-    private final BookServiceImplTemplate bookService;
+    private final UserServiceImpl userService;
+    private final BookServiceImpl bookService;
     private final UserMapper userMapper;
     private final BookMapper bookMapper;
 
-    public UserDataFacade(UserServiceImplTemplate userService,
-                          BookServiceImplTemplate bookService,
+    public UserDataFacade(UserServiceImpl userService,
+                          BookServiceImpl bookService,
                           UserMapper userMapper,
                           BookMapper bookMapper) {
         this.userService = userService;
@@ -59,14 +62,62 @@ public class UserDataFacade {
                 .build();
     }
 
-    public UserBookResponse updateUserWithBooks(UserBookRequest userBookRequest) {
-        return null;
+    public UserBookResponse updateUserWithBooks(UserBookRequest userBookRequest, Long userId) {
+        log.info("Request to update by: {}", userBookRequest);
+        UserDto userDto = userMapper.userRequestToUserDto(userBookRequest.getUserRequest());
+        userDto.setId(userId);
+        log.info("Mapped user request: {}", userDto);
+
+        UserDto updatedUser = userService.updateUser(userDto);
+        log.info("Updated user: {}", updatedUser);
+
+        userService.getAllBooksById(updatedUser.getId())
+                .forEach(book -> bookService.deleteBookById(book.getId()));
+
+        log.info("Updated userId: {} book list", updatedUser.getId());
+        List<Long> bookIdList = userBookRequest.getBookRequests()
+                .stream()
+                .filter(Objects::nonNull)
+                .map(bookMapper::bookRequestToBookDto)
+                .peek(bookDto -> bookDto.setUserId(updatedUser.getId()))
+                .peek(mappedBookDto -> log.info("mapped book: {}", mappedBookDto))
+                .map(bookService::createBook)
+                .peek(createdBook -> log.info("Updated book: {}", createdBook))
+                .map(BookDto::getId)
+                .toList();
+        log.info("Collected book ids: {}", bookIdList);
+
+        return UserBookResponse.builder()
+                .userId(updatedUser.getId())
+                .booksIdList(bookIdList)
+                .build();
     }
 
     public UserBookResponse getUserWithBooks(Long userId) {
-        return null;
+        log.info("Request to got user by id: {}", userId);
+
+        UserDto userById = userService.getUserById(userId);
+
+        List<Long> bookIdList = userService.getAllBooksById(userId).stream()
+                .filter(Objects::nonNull)
+                .peek(filteredBookDto -> log.info("Get book: {} of the user id: {}", filteredBookDto, userId))
+                .map(Book::getId)
+                .toList();
+
+        return UserBookResponse.builder()
+                .userId(userById.getId())
+                .booksIdList(bookIdList)
+                .build();
     }
 
     public void deleteUserWithBooks(Long userId) {
+        log.info("Request to delete user with books, user id: {}", userId);
+
+        userService.getAllBooksById(userId).stream()
+                .filter(Objects::nonNull)
+                .peek(deleteBook -> log.info("Deleted book: {} of the user id: {}", deleteBook, userId))
+                .forEach(deleteBook -> bookService.deleteBookById(deleteBook.getId()));
+
+        userService.deleteUserById(userId);
     }
 }
--- src/main/java/com/edu/ulab/app/repository/UserRepository.java
+++ src/main/java/com/edu/ulab/app/repository/UserRepository.java
@@ -1,11 +1,13 @@
 package com.edu.ulab.app.repository;
 
+import com.edu.ulab.app.entity.Book;
 import com.edu.ulab.app.entity.Person;
 import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import javax.persistence.LockModeType;
+import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends CrudRepository<Person, Long> {
@@ -16,7 +18,10 @@ public interface UserRepository extends CrudRepository<Person, Long> {
     User has books - book - finished
      */
 
-    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    //@Lock(LockModeType.PESSIMISTIC_WRITE)
     @Query("select p from Person p where p.id = :id")
     Optional<Person> findByIdForUpdate(long id);
+
+    @Query("select b from Book b where b.userId = :id")
+    List<Book> findAllBooksById(long id);
 }
